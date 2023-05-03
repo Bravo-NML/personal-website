6d697eea5138c142436b8063c17e9a186d945828
Ne-Miel <nglb.mainpoint@gmail.com>
2023-05-03 14:32:36 +0300

    mobile main

--- src/App.vue
+++ src/App.vue
@@ -21,6 +21,17 @@ export default {
       this.$router.push(a);
     },
   },
+  mounted() {
+    // eslint-disable-next-line
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+
+    window.addEventListener("resize", () => {
+      // We execute the same script as before
+      vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty("--vh", `${vh}px`);
+    });
+  },
 };
 </script>
 
