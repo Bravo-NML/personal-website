8c13ccf7745ffff61515af27ad63962a162a1526
Yana <nglb.mainpoint@gmail.com>
2024-04-30 03:35:06 +0300

    234567

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,12 @@ const { defineConfig } = require("@vue/cli-service");
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  build: {
+    assetsPublicPath: "",
+  },
+  dev: {
+    assetsPublicPath: "/",
+  },
   publicPath:
     process.env.NODE_ENV === "production" ? "/personal-website/" : "/",
 });
